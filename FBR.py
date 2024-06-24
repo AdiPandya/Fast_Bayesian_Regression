0fc69e5211e7191b562195f4dc50c1626ebe3760
AdiPandya <adipandya1510@gmail.com>
2024-06-24 15:14:20 +0200

    Update 2.0: Added features of multiple chains, additional fitting methods of X|Y and ODR and making a trace plot

--- FBR.py
+++ FBR.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 from numba import jit, njit, types, vectorize
+import matplotlib.pyplot as plt
+import seaborn
 
 @njit(nogil=True)
 def ln_likelihood(param, x, y, xerr, yerr, method=0):
@@ -59,7 +61,7 @@ def ln_prior(param, method=0):
             return -np.inf
         return 0.0
     
-    if method ==1:
+    if method == 1:
         if not (-10 < 1/m < 30):
             return -np.inf
         if not (-10 < -c/m < 10):
@@ -122,7 +124,21 @@ def metropolis_step(x, y, xerr, yerr, ln_post_0, theta_0=np.array((0.0, 0.0, 0.0
     return theta_0, ln_post_0
 
 @njit(nogil=True)
-def MCMC(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, method=0, chains=4, n_step_split=True, acc_frac=False):
+def diff(arr):
+    return arr[1:] - arr[:-1]
+
+@njit(nogil=True)
+def any_numba(array):
+    check_array = np.ones(array.shape[0]*array.shape[1])
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            if np.any(array[i,j,:]) == False:
+                check_array[i*array.shape[1]+j] = 0
+                
+    return check_array.reshape(array.shape[0], array.shape[1])
+
+@njit(nogil=True)
+def MCMC(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, method=0, n_chains=1, acc_frac=False):
     """Function that runs the mcmc chain
 
     Args:
@@ -137,21 +153,35 @@ def MCMC(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((
     Returns:
         3 dimensional numpy array: chain of mcmc samples
     """
-    lp0 = ln_posterior(theta_0, x, y, xerr, yerr, method)
-    chain = np.empty((n_steps, len(theta_0), chains))
-    for j in range(chains):
-        for i in range(len(chain)):
-            theta_0, lp0 = metropolis_step(x, y, xerr, yerr, lp0, theta_0, step_cov, method)
-            chain[i] = theta_0
+    if n_chains > 1:
+        n_step_split = True
+    elif n_chains == 1:
+        n_step_split = False
+        
+    if n_step_split == True:
+        n_steps = n_steps//n_chains
+    elif n_step_split == False:
+        n_steps = n_steps
     
-    if acc_frac == True:
-        acc = float(np.any(np.diff(chain, axis=0), axis=1).sum()) / (len(chain)-1)
+    acc = np.zeros(n_chains)
+    chain = np.zeros((n_steps, n_chains,len(theta_0)))
+    
+    for i in range(n_chains):
+        t_0 = theta_0
+        lp0 = ln_posterior(t_0, x, y, xerr, yerr, method)
+        for j in range(n_steps):
+            t_0, lp0 = metropolis_step(x, y, xerr, yerr, lp0, t_0, step_cov, method)
+            chain[j,i,:] = t_0
+    
+    if acc_frac == False:
         return chain, acc
-    else:
-        return chain
+    if acc_frac == True:
+        for i in range(n_chains):
+            acc[i] = any_numba(diff(chain))[:,i].sum() / (len(chain)-1)
+        return chain, acc
   
 @njit(nogil=True)
-def get_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, burn_in=2000):
+def get_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, burn_in=2000, method=0, n_chains=1, acc_frac=False, pow_law=False):
     """Function to get the mean and standard deviation of the parameters from the mcmc chain
 
     Args:
@@ -167,14 +197,36 @@ def get_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.d
     Returns:
         float: (slope, slope std, intercept, intercept std, intrinsic scatter, intrinsic scatter std)
     """
-    chain_0= MCMC(x, y, xerr, yerr, theta_0, step_cov, n_steps)
-    slope, slope_err = chain_0[burn_in:,0].mean(), chain_0[burn_in:,0].std()
-    intercept, intercept_err = chain_0[burn_in:,1].mean(), chain_0[burn_in:,1].std()
-    int_sigma, int_sigma_err = chain_0[burn_in:,2].mean(), chain_0[burn_in:,2].std()
+    chain0,_ = MCMC(x, y, xerr, yerr, theta_0, step_cov, n_steps, method, n_chains, acc_frac)
+    
+    burn_in = burn_in//n_chains
+    chain_0 = np.zeros((chain0[burn_in:,0,].shape[0]*n_chains, chain0[burn_in:,0,].shape[1]))
+    for i in range(n_chains):
+        chain_0[(len(chain0)-burn_in)*i:(len(chain0)-burn_in)*(i+1),:] = chain0[burn_in:,i,:]
+
+    if method == 0 or 2:
+        slope, slope_err = chain_0[:,0].mean(), chain_0[:,0].std()
+        if pow_law == True:
+            Norm = 10**(chain_0[:,1])
+            intercept, intercept_err = Norm.mean(), Norm.std()
+        else:
+            intercept, intercept_err = chain_0[:,1].mean(), chain_0[:,1].std()
+        
+        int_sigma, int_sigma_err = chain_0[:,2].mean(), chain_0[:,2].std()
+        
+    elif method == 1:
+        slope, slope_err = 1/(chain_0[:,0].mean()), (1/chain_0[:,0]).std()
+        if pow_law == True:
+            Norm = 10**(-chain_0[:,1]/chain_0[:,0])
+            intercept, intercept_err = Norm.mean(), Norm.std()
+        else:
+            intercept, intercept_err = -(chain_0[:,1].mean()/chain_0[:,0].mean()), (-chain_0[:,1]/chain_0[:,0].mean()).std()
+        int_sigma, int_sigma_err = chain_0[:,2].mean(), chain_0[:,2].std()
+   
     return slope, slope_err, intercept, intercept_err, int_sigma, int_sigma_err
 
 @njit(nogil=True)
-def get_raw_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, burn_in=2000):
+def get_raw_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, burn_in=2000, method=0, n_chains=1, acc_frac=False, pow_law=False):
     """Function to get the raw values of the parameters from the mcmc chain
 
     Args:
@@ -190,8 +242,168 @@ def get_raw_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=
     Returns:
         float array: slope, intercept, intrinsic scatter values with size (n_steps-burn_in)
     """
-    chain_0= MCMC(x, y, xerr, yerr, theta_0, step_cov, n_steps)
-    slope = chain_0[burn_in:,0]
-    intercept = chain_0[burn_in:,1]
-    int_sigma = chain_0[burn_in:,2]
+    chain0,_ = MCMC(x, y, xerr, yerr, theta_0, step_cov, n_steps, method, n_chains, acc_frac)
+
+    burn_in = burn_in//n_chains
+    chain_0 = np.zeros((chain0[burn_in:,0,].shape[0]*n_chains, chain0[burn_in:,0,].shape[1]))
+    for i in range(n_chains):
+        chain_0[(len(chain0)-burn_in)*i:(len(chain0)-burn_in)*(i+1),:] = chain0[burn_in:,i,:]
+ 
+    if method == 0 or 2:
+        slope = chain_0[:,0]
+        if pow_law == True:
+            intercept = 10**(chain_0[:,1])
+        else:
+            intercept = chain_0[:,1]
+        int_sigma = chain_0[:,2]
+    
+    if method == 1:
+        slope = 1/(chain_0[:,0])
+        if pow_law == True:
+            intercept = 10**(-chain_0[:,1]/chain_0[:,0])
+        else:
+            intercept = -(chain_0[:,1]/chain_0[:,0])
+        int_sigma = chain_0[:,2]
+        
     return slope,  intercept,  int_sigma
+
+@njit(nogil=True)
+def get_a_param(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, burn_in=2000, method=0, n_chains=1, acc_frac=False, pow_law=False):
+    """Function to get the mean and standard deviation of the parameters from the mcmc chain
+
+    Args:
+        x (array): x data
+        y (array): y data
+        xerr (array): error in x
+        yerr (array): error in y
+        theta_0 (float, array): initial values of the parameters (used as mean for the random multivariate draw). Defaults to np.array((0.0, 0.0, 0.0)).
+        step_cov (float, array): diagonal matrix used to control the step size for mcmc. Defaults to np.diag((1e-3, 1e-4, 1e-4)).
+        n_steps (int): Number of mcmc samples. Defaults to 20000.
+        burn_in (int): Number of burn in samples. Defaults to 2000.
+
+    Returns:
+        float: (slope, slope std, intercept, intercept std, intrinsic scatter, intrinsic scatter std)
+    """
+    chain0,_ = MCMC(x, y, xerr, yerr, theta_0, step_cov, n_steps, method, n_chains, acc_frac)
+
+    burn_in = burn_in//n_chains
+    chain_0 = np.zeros((chain0[burn_in:,0,].shape[0]*n_chains, chain0[burn_in:,0,].shape[1]))
+    for i in range(n_chains):
+        chain_0[(len(chain0)-burn_in)*i:(len(chain0)-burn_in)*(i+1),:] = chain0[burn_in:,i,:]
+ 
+    if method == 0 or 2:
+        slope = np.median(chain_0[:,0])
+        slope_u, slope_l = np.percentile(chain_0[:,0], [84, 16])
+        if pow_law == True:
+            Norm = 10**(chain_0[:,1])
+            A = np.median(Norm)
+            A_u, A_l = np.percentile(Norm, [84, 16])
+        else:
+            A = np.median(chain_0[:,1])
+            A_u, A_l = np.percentile(chain_0[:,1], [84, 16])
+        
+        int_sigma = np.median(chain_0[:,2])
+        int_sigma_u, int_sigma_l = np.percentile(chain_0[:,2], [84, 16])
+        
+    elif method == 1:
+        slope = np.median(chain_0[:,0])
+        slope_u, slope_l = np.percentile(1/chain_0[:,0], [84, 16])
+        if pow_law == True:
+            Norm = 10**(-chain_0[:,1]/chain_0[:,0])
+            A = np.median(Norm)
+            A_u, A_l = np.percentile(Norm, [84, 16])
+        else:
+            A = np.median(-chain_0[:,1]/chain_0[:,0])
+            A_u, A_l = np.percentile(-chain_0[:,1]/chain_0[:,0], [84, 16])
+        int_sigma = np.median(chain_0[:,2])
+        int_sigma_u, int_sigma_l = np.percentile(chain_0[:,2], [84, 16])
+   
+    return slope, slope_u, slope_l, A, A_u, A_l, int_sigma, int_sigma_u, int_sigma_l
+
+def plot_trace(x, y, xerr, yerr, theta_0=np.array((0.0, 0.0, 0.0)), step_cov=np.diag((1e-3, 1e-4, 1e-4)), n_steps=20000, burn_in=2000, method=0, n_chains=1, acc_frac=False, pow_law=False, m_chains=False):
+    """Function to get the raw values of the parameters from the mcmc chain
+
+    Args:
+        x (array): x data
+        y (array): y data
+        xerr (array): error in x
+        yerr (array): error in y
+        theta_0 (float, array): initial values of the parameters (used as mean for the random multivariate draw). Defaults to np.array((0.0, 0.0, 0.0)).
+        step_cov (float, array): diagonal matrix used to control the step size for mcmc. Defaults to np.diag((1e-3, 1e-4, 1e-4)).
+        n_steps (int): Number of mcmc samples. Defaults to 20000.
+        burn_in (int): Number of burn in samples. Defaults to 2000.
+
+    Returns:
+        float array: slope, intercept, intrinsic scatter values with size (n_steps-burn_in)
+    """
+    slope, intercept, int_sigma = get_raw_param(x, y, xerr, yerr, theta_0, step_cov, n_steps, burn_in, method, n_chains, acc_frac, pow_law)
+    
+    fig, ax = plt.subplots(3, 2, figsize=(8, 6), sharey='row',gridspec_kw={'width_ratios': [3, 1]})
+
+    chain_0,_ = MCMC(x, y, xerr, yerr, theta_0, step_cov, n_steps, method, n_chains, acc_frac)
+    burn_in = burn_in//n_chains
+    
+    if m_chains == True:
+        chain_0 = chain_0[burn_in:,:,:]
+        slope, intercept, int_sigma = np.zeros((chain_0.shape[0],n_chains)),np.zeros((chain_0.shape[0],n_chains)),np.zeros((chain_0.shape[0],n_chains))
+
+        
+    if m_chains==False:  
+        chain0 = np.zeros((chain_0[burn_in:,0,:].shape[0]*n_chains,chain_0[:,0,:].shape[1]))
+        for i in range(n_chains):
+            chain0[(chain_0.shape[0] - burn_in)*i:(chain_0.shape[0] - burn_in)*(i+1)] = chain_0[burn_in:,i,:]
+        chain_0 = chain0.reshape(chain_0[burn_in:,0,:].shape[0]*n_chains, 1, chain_0.shape[2])  
+        slope, intercept, int_sigma = np.zeros((chain_0.shape[0],1)), np.zeros((chain_0.shape[0],1)), np.zeros((chain_0.shape[0],1))
+    
+    if method == 0 or 2:
+        for i in range(chain_0.shape[1]):
+            slope[:,i] = chain_0[:,i,0]
+            if pow_law == True:
+                intercept[:,i] = 10**(chain_0[:,i,1])
+            else:
+                intercept[:,i] = chain_0[:,i,1]
+            int_sigma[:,i] = chain_0[:,i,2]
+
+    if method == 1:
+        for i in range(chain_0.shape[1]):
+            slope[:,i] = 1/(chain_0[:,i,0])
+            if pow_law == True:
+                intercept[:,i] = 10**(-chain_0[:,i,1]/chain_0[:,i,0])
+            else:
+                intercept[:,i] = -(chain_0[:,i,1]/chain_0[:,i,0])
+            int_sigma[:,i] = chain_0[:,i,2]
+            
+    for i in range(chain_0.shape[1]):
+        ax[0,0].plot(slope[:,i])
+        ax[1,0].plot(intercept[:,i])
+        ax[2,0].plot(int_sigma[:,i])
+        
+        seaborn.kdeplot(y=slope[burn_in:,i], ax=ax[0,1], label=f'Chain {i+1}')
+        seaborn.kdeplot(y=intercept[burn_in:,i], ax=ax[1,1])
+        seaborn.kdeplot(y=int_sigma[burn_in:,i], ax=ax[2,1])
+        
+        ax[2,0].set_xlabel('number of steps')
+        ax[0,0].set_ylabel('Slope')
+        ax[2,0].set_ylabel('Intrinsic scatter')
+        ax[0,1].legend(bbox_to_anchor=(1.9, 1.0))
+        ax[0,1].yaxis.tick_right()
+        ax[1,1].yaxis.tick_right()
+        ax[2,1].yaxis.tick_right()
+        ax[0,1].set_xticklabels([])
+        ax[1,1].set_xticklabels([])
+        ax[2,1].set_xticklabels([])
+        # ax[0,1].yaxis.set_label_position("right")
+        # ax[2,1].yaxis.set_label_position("right")
+        # ax[1,1].yaxis.set_label_position("right")
+        ax[0,1].set_ylabel('Slope', rotation=270, labelpad=20)
+        ax[2,1].set_ylabel('Intrinsic scatter', rotation=270, labelpad=20)
+
+        if pow_law == True:
+            ax[1,0].set_ylabel('Normalization')
+            ax[1,1].set_ylabel('Normalization', rotation=270, labelpad=20)
+        else:
+            ax[1,0].set_ylabel('Intercept')
+            ax[1,1].set_ylabel('Intercept', rotation=270, labelpad=20)
+
+    fig.subplots_adjust(wspace=.05, hspace=.0)
+    return 
